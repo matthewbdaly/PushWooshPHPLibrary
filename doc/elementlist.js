f66ab58a6c6fec5542a79ebe09b1a7a93e7da53b
Matthew Daly <matthewbdaly@gmail.com>
2016-08-03 12:55:34 +0100

    Regenerated docs

--- doc/elementlist.js
+++ doc/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","PushWoosh\\PushWoosh"],["p","PushWoosh\\PushWoosh::$config"],["m","PushWoosh\\PushWoosh::__construct()"],["m","PushWoosh\\PushWoosh::createMessage()"]];
+ApiGen.elements = [["c","PushWoosh\\PushWoosh"]];
